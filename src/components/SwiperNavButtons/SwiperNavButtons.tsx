c0b47a8d267022ff65e88e09c656a22284491175
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2024-04-22 16:40:48 +0100

    first commit

--- src/components/SwiperNavButtons/SwiperNavButtons.tsx
+++ src/components/SwiperNavButtons/SwiperNavButtons.tsx
@@ -1,16 +1,20 @@
 import { useSwiper } from "swiper/react";
-import './SwiperNavButtons.css'
 import ArrowForwardCircular from "../ArrowForwardCircular/ArrowForwardCircular";
-import ArrowLeftCircular from "../ArrowForwardCircular/ArrowForwardCircular";
+import ArrowLeftCircular from "../ArrowLeftCircular/ArrowLeftCircular";
+import "./SwiperNavButtons.css";
 
-export function SwiperNavButtons () {
-    const swipernav = useSwiper()
-    const back =<ArrowLeftCircular/>
-    const front= <ArrowForwardCircular/>
-    return(
-        <div className="swiper-buttons">
-        <button className="custom-prev-btn" onClick={()=>swipernav.slidePrev()}>{back}</button>
-        <button className="custom-next-btn" onClick={()=>swipernav.slideNext()}>{front}</button>
-        </div>
-    )
+export function SwiperNavButtons() {
+  const swipernav = useSwiper();
+  const back = <ArrowLeftCircular />;
+  const front = <ArrowForwardCircular />;
+  return (
+    <div className="swiper-buttons">
+      <button className="custom-prev-btn" onClick={() => swipernav.slideNext()}>
+        {back}
+      </button>
+      <button className="custom-next-btn" onClick={() => swipernav.slidePrev()}>
+        {front}
+      </button>
+    </div>
+  );
 }
--- src/pages/HomePage/components/TopCategory/TopCategory.tsx
+++ src/pages/HomePage/components/TopCategory/TopCategory.tsx
@@ -1,46 +1,44 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/bundle'
-import { SwiperNavButtons } from '../../../../components/SwiperNavButtons/SwiperNavButtons';
-import { Navigation} from 'swiper/modules';
-import TopCategoryData from "./TopCategoryData"
+import "swiper/css";
+import "swiper/css/bundle";
+import { Navigation } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { SwiperNavButtons } from "../../../../components/SwiperNavButtons/SwiperNavButtons";
+import TopCategoryData from "./TopCategoryData";
 
 function TopCategory() {
-
   interface TopCategoryDataTypes {
     img: string;
     type: string;
     amount: number;
-}
+  }
 
-  function card(props: TopCategoryDataTypes){
-    const {img, type,amount} = props
-    return(
+  function card(props: TopCategoryDataTypes) {
+    const { img, type, amount } = props;
+    return (
       <SwiperSlide>
-      <div className=" px-4 text-center d-flex flex-column align-items-center">
-      <div className="category_icon d-flex align-items-center justify-content-center rounded-circle mb-1">
-        <img src={img} alt="real estate office" />
-      </div>
-      <p className="mb-0 fw-bold">{type}</p>
-      <p className="mb-0 fw-medium">({amount})</p>
-    </div>
-    </SwiperSlide>
-    )
+        <div className=" px-4 text-center d-flex flex-column align-items-center">
+          <div className="category_icon d-flex align-items-center justify-content-center rounded-circle mb-1">
+            <img src={img} alt="real estate office" />
+          </div>
+          <p className="mb-0 fw-bold">{type}</p>
+          <p className="mb-0 fw-medium">({amount})</p>
+        </div>
+      </SwiperSlide>
+    );
   }
 
   return (
     <Swiper
-    spaceBetween={50} 
-    slidesPerView={5} 
-    loop={true}
-    modules={[Navigation]}
-    onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')}
-    className="performance_metrics m-auto py-4 rounded-pill d-flex align-items-center justify-content-between px-5"
+      spaceBetween={50}
+      slidesPerView={3}
+      loop={true}
+      modules={[Navigation]}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="performance_metrics m-auto py-4 rounded-pill d-flex align-items-center justify-content-between px-5"
     >
-      <SwiperNavButtons/>
+      <SwiperNavButtons />
       {TopCategoryData.map(card)}
-      
     </Swiper>
   );
 }
