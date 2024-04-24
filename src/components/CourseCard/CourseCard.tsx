b6ddf3d0d54aa0552d201048bef63a5f08b04a24
Okwor Chukwuemeka Nnajiofor <88171212+thenaijadev@users.noreply.github.com>
2024-04-24 08:26:54 +0100

    Fix-UI Fixes

--- src/components/CourseCard/CourseCard.tsx
+++ src/components/CourseCard/CourseCard.tsx
@@ -1,42 +1,46 @@
-import { SwiperSlide } from "swiper/react"
-import { ArrowFowardSmall, Star } from "../../assets"
-import IconButton from "../IconButon/IconButton"
-import MyBadge from "../MyBadge/MyBadge"
-import './CourseCard.css'
+import { SwiperSlide } from "swiper/react";
+import { ArrowFowardSmall, Star } from "../../assets";
+import IconButton from "../IconButon/IconButton";
+import MyBadge from "../MyBadge/MyBadge";
+import "./CourseCard.css";
 
 type Props = {
   title: string;
   author: string;
   amount: number;
-  review: number
-}
+  review: number;
+};
 
 const CourseCard = (props: Props) => {
   return (
-    <SwiperSlide className="card border border-black rounded-4 mb-5 py-3 px-3">
-              <div className="rounded-3 roof_top_container pe-3 pt-2">
-                <div className="d-flex justify-content-end  ">
-                  <MyBadge name="Expert"/>
-                </div>
-              </div>
-              <div className="card-body m-auto px-0">
-                <div className="d-flex gap-3 align-items-center ">
-                  <MyBadge name="Real Estate"/>
-                  <div className="d-flex">
-                    <img src={Star} alt="star" />
-                    <p className="mb-0 fw-medium">{`${props.review} reviews`}</p>
-                  </div>
-                </div>
-                <h5 className="card-title my-3">{props.title}</h5>
-                <p className="card-text">{props.author}</p>
-                <div className="d-flex justify-content-between align-items-center enroll">
-                   <IconButton title="Enroll Now" image={ArrowFowardSmall}/>
-                  <p className="card-text"><small className="text-body-secondary">{`N${props.amount}`}</small>
-                  </p>
-                </div>
-              </div>
-            </SwiperSlide>       
-  )
-}
+    <SwiperSlide
+      className="card border border-black rounded-4 mb-5 py-3 px-3"
+      style={{ transform: "scale(1)" }}
+    >
+      <div className="rounded-3 roof_top_container pe-3 pt-2">
+        <div className="d-flex justify-content-end  ">
+          <MyBadge name="Expert" />
+        </div>
+      </div>
+      <div className="card-body m-auto px-0">
+        <div className="d-flex gap-3 align-items-center ">
+          <MyBadge name="Real Estate" />
+          <div className="d-flex">
+            <img src={Star} alt="star" />
+            <p className="mb-0 fw-medium">{`${props.review} reviews`}</p>
+          </div>
+        </div>
+        <h5 className="card-title my-3">{props.title}</h5>
+        <p className="card-text">{props.author}</p>
+        <div className="d-flex justify-content-between align-items-center enroll">
+          <IconButton title="Enroll Now" image={ArrowFowardSmall} />
+          <p className="card-text">
+            <small className="text-body-secondary">{`N${props.amount}`}</small>
+          </p>
+        </div>
+      </div>
+    </SwiperSlide>
+  );
+};
 
-export default CourseCard
+export default CourseCard;
--- src/pages/HomePage/components/AllCourse.tsx
+++ src/pages/HomePage/components/AllCourse.tsx
@@ -1,26 +1,26 @@
-import { Swiper} from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/bundle'
-import { Navigation} from 'swiper/modules';
-import CourseCard from '../../../components/CourseCard/CourseCard'
-import AllCourseData from '../../../components/CourseCard/CourseData'
-import { SwiperNavButtons } from '../../../components/SwiperNavButtons/SwiperNavButtons'
+import "swiper/css";
+import "swiper/css/bundle";
+import { Navigation } from "swiper/modules";
+import { Swiper } from "swiper/react";
+import CourseCard from "../../../components/CourseCard/CourseCard";
+import AllCourseData from "../../../components/CourseCard/CourseData";
+import { SwiperNavButtons } from "../../../components/SwiperNavButtons/SwiperNavButtons";
 const AllCourse = () => {
   return (
-           <Swiper
-    spaceBetween={50} 
-    slidesPerView={5} 
-    loop={true}
-    modules={[Navigation]}
-    onSwiper={(swiper) => console.log(swiper)}
-    onSlideChange={() => console.log('slide change')}
-    className="d-flex justify-content-around align-items-center gap-3 mb-5 m-auto"
+    <Swiper
+      spaceBetween={20}
+      slidesPerView={4}
+      loop={true}
+      style={{ transform: "scale(0.9)" }}
+      modules={[Navigation]}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log("slide change")}
+      className="d-flex justify-content-around align-items-center gap-3 mb-5 m-auto"
     >
-      <SwiperNavButtons/>
+      <SwiperNavButtons />
       {AllCourseData.map(CourseCard)}
-      
     </Swiper>
-  )
-}
+  );
+};
 
-export default AllCourse
+export default AllCourse;
